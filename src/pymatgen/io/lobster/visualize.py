b58542af8c9a34b4ae38911fc55842240e698234
kueltzen <kueltzen@VDI0139.zit.bam.de>
2026-01-12 17:24:54 +0100

    Added class method for instantiating lobsterneighborsvisualizer from structure and lobster output.

--- src/pymatgen/io/lobster/visualize.py
+++ src/pymatgen/io/lobster/visualize.py
@@ -1,10 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from pymatgen.io.lobster import LobsterNeighbors
-    from pymatgen.util.typing import PathLike
+from pymatgen.io.lobster.lobsterenv import LobsterNeighbors
 
 
 class LobsterNeighborsVisualizer:
@@ -14,48 +10,41 @@ class LobsterNeighborsVisualizer:
     """
 
     def __init__(
-        self, lobster_neighbors: LobsterNeighbors, bond_tol: float = 0.1, file_name: str = "output.vesta"
+        self,
+        lobster_neighbors: LobsterNeighbors,
+        bond_tol: float = 0.1,
     ) -> None:
         """
         Args:
             lobsterneighbors : pymatgen.io.lobster.LobsterNeighbors
                 LobsterNeighbors object used to determine bonding.
             bond_tol : float
-                Tolerance added/subtracted from bond lengths for VESTA SBOND entries.
-            file_name : str
-                Output VESTA file name.
+                Tolerance added/subtracted from actual bond lengths for VESTA SBOND entries.
         """
         self.bond_tol = bond_tol
-        self.file_name = file_name
         self.lobster_neighbors = lobster_neighbors
-        self.bonds = self._get_relevant_bonds()
+        self._bonds = self._get_relevant_bonds()
 
     @classmethod
     def from_lobster_calc(
         cls,
-        lobster_path: PathLike,
-        structure_file: str = "POSCAR",
-        icohp_file: str = "ICOHPLIST.lobster",
+        bond_tol: float = 0.1,
+        *args,
+        **kwargs,
     ):
         """
-        Construct a LobsterNeighborsPlotter from a LOBSTER calculation directory.
+        Construct a LobsterNeighborsVisualizer object from a structure and LOBSTER calculation output.
 
         Args:
-            lobster_path : str or Path
-                Path to the LOBSTER calculation directory.
-            bond_tol : float
-                Tolerance added/subtracted from bond lengths for VESTA SBOND entries.
-            file_name : str
-                Output VESTA file name.
-            structure_file : str, optional
-                Structure file used by LOBSTER (default: "POSCAR").
-            icohp_file : str, optional
-                ICOHP list file produced by LOBSTER (default: "ICOHPLIST.lobster").
-
+             bond_tol : float
+                Tolerance added/subtracted from actual bond lengths for VESTA SBOND entries.
+            *args, **kwargs
+                args and kwargs supported by LobsterNeighbors init
         Returns:
-            LobsterNeighborsPlotter
+            LobsterNeighborsVisualizer
         """
-        # TODO
+        lobster_neighbors = LobsterNeighbors(*args, **kwargs)
+        return cls(lobster_neighbors=lobster_neighbors, bond_tol=bond_tol)
 
     def _get_relevant_bonds(self) -> list:
         bonds = []
@@ -68,18 +57,20 @@ class LobsterNeighborsVisualizer:
                 if [site_to_str, site_str, length] not in bonds:
                     # TODO advanced: get bond lengths of relevant and not-relevant ICOHPS and
                     # define meaningful distance intervals for vesta
-                    # TODO advanced: bond thickness in vesta as a function of ICOHP values?
                     bonds.append([site_str, site_to_str, length])
         return bonds
 
-    def write_vesta(self) -> None:
+    def write_vesta(self, file_name: str = "output.vesta") -> None:
         """
         Write a VESTA file with bonds defined as per LOBSTER calculation output.
 
+        Args:
+            file_name : str
+                Output VESTA file name.
         """
         # partly adapted fr. Janine George:
         # https://github.com/materialsproject/pymatgen/blob/682bfd855dd89264b0ff8d6bee4815f8834cc5ac/src/pymatgen/phonon/thermal_displacements.py#L310
-        with open(self.file_name, mode="w", encoding="utf-8") as file:
+        with open(file_name, mode="w", encoding="utf-8") as file:
             file.write("#VESTA_FORMAT_VERSION 3.5.4\n \n \n")
             file.write("CRYSTAL\n\n")
             file.write("TITLE\n")
